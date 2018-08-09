cbb4499200fadc2bca0d2da6378780da30f9f799
liverbool <nukboon@gmail.com>
2018-08-09 20:38:15 +0700

    bc, sf4

--- common/common.php
+++ common/common.php
@@ -198,6 +198,9 @@ task('common:build_parameters', function () {
 
     run("mkdir -p {{deploy_path}}/shared/app/config");
     run('echo "' . $newParameters . '" > {{deploy_path}}/shared/app/config/parameters.yml');
+
+    // sf4
+    run('echo "' . $newParameters . '" > {{release_path}}/config/parameters.yml');
 })->setPrivate();
 
 /**
