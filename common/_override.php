b3028772b607649a6a5dbde3227fbc668468d5e5
liverbool <nukboon@gmail.com>
2019-02-28 18:47:33 +0700

    bugfix

--- common/_override.php
+++ common/_override.php
@@ -5,22 +5,6 @@ use function Deployer\{
 };
 
 /**
- * Normalize asset timestamps
- */
-task('deploy:assets', function () {
-    $assets = implode(' ', array_map(function ($asset) {
-        return "{{release_path}}/$asset";
-    }, get('assets')));
-
-    $time = date('Ymdhi.s');
-
-    run("find $assets -exec touch -t $time {} ';' &> /dev/null || true");
-
-    // BUGFIX: liip imagine need image folder
-    run("mkdir -p {{deploy_path}}/shared/web/media/image");
-})->desc('Normalize asset timestamps');
-
-/**
  * Adding not override!
  * Copy directories. Useful for vendors directories
  */
